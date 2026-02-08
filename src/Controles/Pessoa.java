901943546f411ab03d621ff70459bb91a7edaeac
VINICIUS GONCALVES MESQUITA <vinicius.goncalves2503@gmail.com>
2026-02-08 03:17:31 -0300

    Create Pessoa.java

---
+++ src/Controles/Pessoa.java
@@ -0,0 +1,225 @@
+package Controles;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.*; // Importa tudo de javax.persistence
+
+@Entity
+@Table(name = "pessoa")
+@NamedQueries({
+    @NamedQuery(name = "Pessoa.findAll", query = "SELECT p FROM Pessoa p"),
+    @NamedQuery(name = "Pessoa.findByCodigo", query = "SELECT p FROM Pessoa p WHERE p.codigo = :codigo"),
+    @NamedQuery(name = "Pessoa.pesquisaNome", query = "SELECT p FROM Pessoa p WHERE p.nome LIKE :nome")
+})
+
+public class Pessoa implements Serializable {
+
+    @OneToMany(mappedBy = "pessoa")
+    private Collection<Emprestimo> emprestimoCollection;
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Informa que o BD gera o ID
+    @Basic(optional = false)
+    @Column(name = "codigo")
+    private Integer codigo;
+
+    @Basic(optional = false)
+    @Column(name = "nome")
+    private String nome;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "apelido")
+    private String apelido;
+
+    @Basic(optional = false)
+    @Column(name = "tipo_pessoa")
+    private Character tipoPessoa;
+
+    @Column(name = "cpf")
+    private String cpf;
+
+    @Column(name = "rg")
+    private String rg;
+
+    @Column(name = "cnpj")
+    private String cnpj;
+
+    @Column(name = "inscricao_estadual")
+    private String inscricaoEstadual;
+
+    @Column(name = "endereco")
+    private String endereco;
+
+    @Column(name = "numero")
+    private String numero;
+
+    @Column(name = "bairro")
+    private String bairro;
+
+    @Column(name = "cidade")
+    private String cidade;
+
+    @Column(name = "uf")
+    private String uf;
+
+    @Column(name = "cep")
+    private String cep;
+
+    public Pessoa() {
+    }
+
+    // GETTERS E SETTERS
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getApelido() {
+        return apelido;
+    }
+
+    public void setApelido(String apelido) {
+        this.apelido = apelido;
+    }
+
+    public Character getTipoPessoa() {
+        return tipoPessoa;
+    }
+
+    public void setTipoPessoa(Character tipoPessoa) {
+        this.tipoPessoa = tipoPessoa;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getInscricaoEstadual() {
+        return inscricaoEstadual;
+    }
+
+    public void setInscricaoEstadual(String inscricaoEstadual) {
+        this.inscricaoEstadual = inscricaoEstadual;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    // Métodos hashCode, equals e toString (geralmente gerados pela IDE)
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (codigo != null ? codigo.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (!(object instanceof Pessoa)) {
+            return false;
+        }
+        Pessoa other = (Pessoa) object;
+        return !((this.codigo == null && other.codigo != null) || (this.codigo != null && !this.codigo.equals(other.codigo)));
+    }
+
+    @Override
+    public String toString() {
+        return "Controles.Pessoa[ codigo=" + codigo + " ]";
+    }
+
+    public Collection<Emprestimo> getEmprestimoCollection() {
+        return emprestimoCollection;
+    }
+
+    public void setEmprestimoCollection(Collection<Emprestimo> emprestimoCollection) {
+        this.emprestimoCollection = emprestimoCollection;
+    }
+}
